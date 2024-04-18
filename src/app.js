0d5b3797787f79bc1020c36831e6e10a545892f8
RomiBeak <163849611+RomiBeak@users.noreply.github.com>
2024-04-18 14:51:44 +0000

    romi

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const excuses = {
+  intro: ["The dog", "My grandma", "The mailman", "My bird"],
+  action: ["ate", "peed", "crushed", "broke"],
+  what: ["my homework", "y phone", "the car"],
+  when: [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ]
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const randomElement = arr => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const randomExcuse = () => {
+  const intro = randomElement(excuses.intro);
+  const action = randomElement(excuses.action);
+  const what = randomElement(excuses.what);
+  const when = randomElement(excuses.when);
+
+  const excuseMessage = `${intro} ${action} ${what} ${when}`;
+  excuses.innerHTML = excuseMessage;
+};
+
+window.onload = () => {
+  const button = document.querySelector("#excuse-button");
+  const excuse = document.querySelector("#excuse");
+
+  button.addEventListener("click", randomExcuse);
 };
